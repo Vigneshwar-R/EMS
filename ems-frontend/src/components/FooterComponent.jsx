e49d97479dd3c8cc37467c0e65b78529817be49d
Vigneshwar-R <r.vigneshwar2000@gmail.com>
2025-03-17 11:49:40 -0700

    Add files via upload

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className = 'footer' >
+            <p>*Est Choodo 2024*</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
